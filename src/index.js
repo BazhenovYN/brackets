53b1b93a71149aa98c71a627042d1e7b3500b538
BazhenovYN <BazhenovYN@gmail.com>
2023-02-11 14:23:15 +0300

    feat: done brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const matching = {};
+  const openBrackets = [];
+  bracketsConfig.forEach(element => {
+    matching[element[1]] = element[0];
+    openBrackets.push(element[0]);
+  });
+
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+
+    const curentBracket = str[i];
+    
+    if (stack.length > 0) {
+      const top = stack[stack.length - 1];
+      if (matching[curentBracket] === top) {
+        stack.pop();
+        continue;
+      }
+    }
+
+    if (openBrackets.includes(curentBracket)) {
+      stack.push(curentBracket);
+    } else {
+      return false;
+    }
+
+  }
+  return stack.length === 0;
 }
